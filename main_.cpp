dc13dda50802d2ff9772e11f80fb367edc98b139
NikitaKasianenko <nikkas2016@gmail.com>
2024-06-27 20:20:17 +0300

    Rebuld main

--- main_.cpp
+++ main_.cpp
@@ -47,7 +47,7 @@ public:
             exit(1);
         }
         sprintf(path, "c:\\windows\\temp\\%s.txt", input);
-        free(input);  
+        free(input);
         return path;
     }
 };
@@ -262,58 +262,67 @@ int main() {
     size_t bufferSize = 256;
     Text array;
     TextEditor editor;
+    CaesarCipher cipher;
     char* command = nullptr;
     char* path_in = nullptr;
     char* path_out = nullptr;
+    int key = 0;
+    int nrow = 0;
+    char** buffer = nullptr;
 
     while (true) {
         printf("Encrypt - 1, Decrypt - 2, Print state - 3, Exit - 4 \nCommand: ");
         command = CLI::user_input(&bufferSize);
 
-        if (strcmp(command, "4") == 0) {
-            break;
-        }
+        switch (atoi(command)) {
+        case 4: // Exit
+            free(command);
+            return 0;
 
-        else if (strcmp(command, "3") == 0) {
+        case 3: // Print state
             editor.print(array);
+            free(command);
             continue;
-        }
 
-        printf("Enter file name to open: \n");
-        path_in = CLI::user_file(&bufferSize);
-
-        if (strcmp(command, "1") == 0 || strcmp(command, "2") == 0) {
+        case 1: // Encrypt
+        case 2: // Decrypt
+            printf("Enter file name to open: \n");
+            path_in = CLI::user_file(&bufferSize);
 
             printf("Enter key: \n");
-            int key = atoi(CLI::user_input(&bufferSize));
+            key = atoi(CLI::user_input(&bufferSize));
             printf("Enter file name to save: \n");
             path_out = CLI::user_file(&bufferSize);
 
-            int nrow;
-            char** buffer = FileHandler::read_from_file(path_in, bufferSize, &nrow);
+            buffer = FileHandler::read_from_file(path_in, bufferSize, &nrow);
             if (buffer == nullptr) {
                 printf("Error reading from file.\n");
                 free(path_in);
                 free(path_out);
+                free(command);
                 continue;
             }
             array.setArray(buffer);
             array.setNrow(nrow);
 
-            CaesarCipher cipher;
+            
             for (int i = 0; i < nrow; i++) {
-                if (strcmp(command, "1") == 0) {
+                if (atoi(command) == 1) {
                     array.getArray()[i] = cipher.encrypt_text(array.getArray()[i], key);
                 }
-                else if (strcmp(command, "2") == 0) {
+                else if (atoi(command) == 2) {
                     array.getArray()[i] = cipher.decrypt_text(array.getArray()[i], key);
                 }
             }
 
             FileHandler::write_in_file(path_out, array, nrow);
             free(path_out);
-        }
+            break;
 
+        default:
+            printf("Invalid command.\n");
+            break;
+        }
 
         free(command);
         free(path_in);
