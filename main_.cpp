c9e6693ec096796a769d6b7fd3a4697ec3cad877
NikitaKasianenko <nikkas2016@gmail.com>
2024-06-27 20:04:21 +0300

    Text Class

--- main_.cpp
+++ main_.cpp
@@ -52,3 +52,105 @@ public:
     }
 };
 
+class Text {
+public:
+    Text() {
+        buffersize = 256;
+        initialrowcount = 10;
+        nrow = 0;
+        ncol = 0;
+        array = nullptr;
+        initialize_array();
+    }
+
+    ~Text() {
+        freeArray();
+    }
+
+    char** getArray() const {
+        return array;
+    }
+
+    void setArray(char** newArray) {
+        freeArray();
+        array = newArray;
+        nrow = 0;
+        if (newArray != nullptr) {
+            while (newArray[nrow] != nullptr) {
+                nrow++;
+            }
+        }
+    }
+
+    int getNrow() const {
+        return nrow;
+    }
+
+    void setNrow(int newNrow) {
+        nrow = newNrow;
+    }
+
+    int getNCol() const {
+        return ncol;
+    }
+
+    void setNCol(int newNcol) {
+        ncol = newNcol;
+    }
+
+    void initialize_array() {
+        array = (char**)malloc(initialrowcount * sizeof(char*));
+        if (array == nullptr) {
+            printf("memory allocation failed.");
+            exit(1);
+        }
+        for (int i = 0; i < initialrowcount; i++) {
+            array[i] = (char*)malloc(buffersize * sizeof(char));
+            if (array[i] == nullptr) {
+                printf("memory allocation failed.");
+                exit(1);
+            }
+            array[i][0] = '\0';
+        }
+    }
+
+    void freeArray() {
+        if (array) {
+            for (int i = 0; i < initialrowcount && array[i] != nullptr; i++) {
+                free(array[i]);
+            }
+            free(array);
+            array = nullptr;
+        }
+    }
+
+    void reallocate_rows() {
+        initialrowcount *= 2;
+        char** temp = (char**)realloc(array, initialrowcount * sizeof(char*));
+        if (temp == nullptr) {
+            printf("memory allocation failed.");
+            exit(1);
+        }
+        array = temp;
+        for (int i = nrow; i < initialrowcount; i++) {
+            array[i] = (char*)malloc(buffersize * sizeof(char));
+            if (array[i] == nullptr) {
+                printf("memory allocation failed.");
+                exit(1);
+            }
+            array[i][0] = '\0';
+        }
+    }
+
+    void newbuffer(size_t* buffersize) {
+        *buffersize *= 2;
+    }
+
+protected:
+    size_t buffersize;
+    int initialrowcount;
+    int nrow;
+    int ncol;
+    char** array;
+};
+
